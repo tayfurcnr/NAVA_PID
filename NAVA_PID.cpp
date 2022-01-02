5bd5918e1a84a4c2f8559f12383793c786ec2ca3
Tayfur Çınar <67666933+tayfurcnr@users.noreply.github.com>
2022-01-03 02:31:33 +0300

    Beginner Level

---
+++ NAVA_PID.cpp
@@ -0,0 +1,56 @@
+#include <NAVA_PID.H>
+
+NAVA_PID::NAVA_PID(double* desired_degree, double* measured_degree, double* throttle, double Kp, double Ki, double Kd)
+{
+    my_desired_degree = desired_degree;
+    my_measured_degree = measured_degree;
+    my_throttle = throttle;
+    my_kp = Kp;
+    my_ki = Ki;
+    my_kd = Kd;
+}
+
+void NAVA_PID::compute()
+{
+  current_time = millis();
+  elapsed_time = (current_time - prev_time) / 1000;
+
+  error = my_measured_degree - my_desired_degree;
+  
+  pid_p = my_kp * error;
+
+  if(-3 < error <3)
+  {
+    pid_i = pid_i + (my_ki * error);  
+  }
+
+  pid_d = my_kd * ((error - prev_error) / elapsed_time);
+
+  pid = pid_p + pid_i + pid_d;
+
+  if(pid < -1000)
+     pid = -1000;
+  if(pid >  1000)
+     pid =  1000;
+
+  pwm_right = throttle - pitch_pid;
+  pwm_left = throttle + pitch_pid;
+
+  if(pwm_right < 1000)
+     pwm_right = 1000;
+  if(pwm_right > 2000)
+     pwm_right = 2000;
+
+  if(pwm_left < 1000)
+     pwm_left = 1000;
+  if(pwm_left > 2000)
+     pwm_left
+
+  prev_error = error;
+
+  prev_time = current_time;
+}
+
+double NAVA_PID::get_pwm_right() {return pwm_right;}
+
+double NAVA_PID::get_pwm_left() {return pwm_left;}
---
+++ NAVA_PID.h
@@ -0,0 +1,36 @@
+#ifndef NAVA_PID
+#define NAVA_PID
+#define LIBRARY_VERSION	1.1
+
+class NAVA_PID
+{
+  public:
+
+    NAVA_PID(double*, double*, double*,double, double, double);
+
+    void compute();
+
+    double get_pwm_right();
+    double get_pwm_left();
+
+  private:
+	double pid, pwm_right, pwm_left, error, prev_error;
+
+	double pid_p = 0;
+	double pid_i = 0;
+	double pid_d = 0;
+    
+	double my_kp;                  // * (P)roportional Tuning Parameter
+    double my_ki;                  // * (I)ntegral Tuning Parameter
+    double my_kd;                  // * (D)erivative Tuning Parameter
+
+    double *my_desired_degree;
+    double *my_measured_degree;
+    double *my_throttle;
+	
+	unsigned long elapsed_time;	  
+	unsigned long current_time;
+	unsigned long prev_time = 0;
+};
+#endif
+
